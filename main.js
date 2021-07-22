ae2212599e5a4f2c06ea6ef0e2f9da0d071bfe38
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-07-22 16:20:27 -0500

    add/ index.html & main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const natalia = {
+  name: "Natalia",
+  age: 20,
+  cursosAprobados: [
+    "Curso definitivo de HTML y CSS",
+    "Curso de POO"
+  ]
+};
+
+// Natalia apruebe otro curso
+console.log(natalia);
+natalia.cursosAprobados.push("Curso de Responsive Design");
+console.log(natalia);
