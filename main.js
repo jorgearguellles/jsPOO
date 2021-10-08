a1693876b5a9f1324cb10b8c8ac991150d92e7a8
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-10-07 21:04:24 -0500

    add/ notas sobre jsPOO :)

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+/* ==========================================================================
+ * Clase: Objetos literales vs Prototipos en JS
+ * ==========================================================================
+ */ 
+
 // Primer objeto literal -> const natalia = {...};
 const natalia = {
   name: "Natalia",
@@ -27,11 +32,58 @@ const natalia = {
 
 // Nuestro Primer Prototipo -> 
 
-function student(name, age, cursosAprobados1){
+function Student(name, age, cursosAprobados1){
   this.name = name,
   this.age = age,
-  this.cursosAprobados1  = cursosAprobados1,
+  this.cursosAprobados1  = cursosAprobados1
+  // Obción uno para crear métodos al interior de los prototipos:
+  // this.aprobarCurso2 = function(nuevoCurso){
+  //   this.cursosAprobados1.push(nuevoCurso);
+  // }
+  // Obción dos para crear métodos al interior de los prototipos: Por fuera de la función...
+};
 
+Student.prototype.cursosAprobados2 = function(nuevoCurso){
+  this.cursosAprobados1.push(nuevoCurso);
+};
+
+// Creemos una instacia de nuestro primer portotipo
+
+const juanita = new Student(
+  "Juanita Alejandra",
+  15,
+  ["Curso Introducción a los Video juegos", "Curso de Creación de personaje"],
+);
+
+
+/* ==========================================================================
+ * Clase: Clases en JS - Prototipos con la sintaxis de clase
+ * ==========================================================================
+ */
+
+
+class Student2{
+
+  constructor(name, age, cursosAprobados1){
+    this.name2 = name,
+    this.age2 = age,
+    this.cursosAprobados2  = cursosAprobados1
+  }
+
+  aprobarCurso2(curso2){
+    this.cursosAprobados2.push(curso2);
+  }
 }
 
+const miguelito = new Student2(
+  "Miguel",
+  28,
+  [
+    "Curso de analisis de negocios para ciencia de datos",
+    "Principio de Data Viz para Businees admin"
+  ]
+)
 
+console.log(miguelito);
+miguelito.aprobarCurso2("Curso de cocina");
+console.log(miguelito);
