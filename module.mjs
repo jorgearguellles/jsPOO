69272128c48b635724093c35ad5765a6e12c9f8f
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-12-14 13:24:50 +0100

    add/ clases de encapsulamiento aterrizado con ejemplos

---
+++ module.mjs
@@ -0,0 +1,15 @@
+import { PlatziClass } from './main.mjs'
+
+const clase75 = new PlatziClass({
+  name: "Clase introductoria",
+  videoID: "12345",
+});
+
+clase75.reproducir();
+clase75.pausar();
+
+clase75.reproducir();
+clase75.pausar();
+
+clase75.reproducir();
+clase75.pausar();
